1c7cf66c72e56a344934e3d97e3ebba070ca70d0
Aaron Arruda <aaron@abarruda.com>
2016-09-28 00:27:47 -0700

    Updating setup with nginx config file location

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ sudo pip install flask tornado configparser
 # TODO:
 # Install RPi-Cam-Web-Interface
 
-# Add the following to nginx config file ()
+# Add the following to nginx config file (/etc/nginx/sites-enabled/rpicam)
 # This makes available the live preview outside of the authentication
 # necessary for the RPi-Cam-Web-Interface management interface
 
