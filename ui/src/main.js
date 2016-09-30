9c2459e52c55ff22c34a244b47c506e8ff88143f
Aaron Arruda <aaron@abarruda.com>
2016-09-29 23:16:31 -0700

    Connected UI to API for status and toggling.

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,8 +1,11 @@
 "use strict";
 var React = require('react');
 var ReactDOM = require('react-dom');
+var $ = require('jquery');
+var Alert = require('react-bootstrap').Alert;
 var Button = require('react-bootstrap').Button;
 var Col = require('react-bootstrap').Col;
+var Glyphicon = require('react-bootstrap').Glyphicon;
 var Grid = require('react-bootstrap').Grid;
 var Image = require('react-bootstrap').Image;
 var PageHeader = require('react-bootstrap').PageHeader;
@@ -15,15 +18,19 @@ var garageOpenData = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYA
 var garageClosedData = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAB10lEQVRoQ+2a0U3EMBBE5yqADqADoALoAKgAOqAWOoAOoAOoAKgASoAKDr3IRnsmjhzHUSBaS/dxkr2ZNzu3H+dstJK1WQmH5gQ5lnQl6SKY9SDpXtLrHOa1BjkPwhG/nxH8KQkoPo+toKaCIPY0I/5N0pOkuyD2WtKZpCMj3kI9S+J71aoBQbx13j4Y8QjH7Y+MosMADpiFYrvt1CioUhAejngcjZmPOokHzg+Jz7kcoahLfbuoR13q50z52T8EEsXjHD9cuyge3Rvl3EBu6DQm8UmhGBB0OguVgsRJg0NW/JcRjkutxOe4gIrdB2zPbAQKDTsT0IK8DIjH/SVX7FQf1AnCLMg2KL0NbZxl3jdwg6QQ95tQq2PoAykdAA00TSoRjXeQSTY2POwdaWhmk1KjO5KO5SYqRhZh/F8mZ0aDxAMjn918ezpNq0GWGss7go09DuIdmfhr8WgNToeJ7tYc9454R2pyU3DGo+XRKohJzRaPlkerJjcFZzxaHq2CmNRs8Wh5tGpyU3DGo+XRKohJzZZFo/Uuiau7vsW9S3c5U7gWBeHy8iAjlJvf9E5yiGlRkEKzi7Y5SJFNf2jTr6s3rnx5HeM/Le7duxcYlvo/t7lZqwH5BmPFwjM4vQU4AAAAAElFTkSuQmCC";
 
 var imageSrcUrl = "http://" + window.globalConfigs.server + "/preview";
+var apiUrl = "http://" + window.globalConfigs.server + ":" + window.globalConfigs.apiPort + "/garage-door-control/api/v1/";
 
 var Enclosure = React.createClass({
 
 	getInitialState: function() {
 		return {
-			state: "CLOSED",
+			closed: true,
+			opened: false,
 			key: 1, 
 			url: imageSrcUrl,
-			toggle: false};
+			alertApiStatusError: false,
+			alertApiToggleError: false,
+			alertSensorMalfunction: false};
 	},
 
 	refreshPicture: function () {
@@ -32,25 +39,105 @@ var Enclosure = React.createClass({
 
 	componentDidMount: function() {
 		setInterval(this.refreshPicture, 5000);
+		setInterval(this.loadStatus, 2000);
+	},
+
+	loadStatus: function() {
+		$.ajax({
+			url: apiUrl + "status",
+			cache: false,
+			timeout: 5000,
+			success: function(data) {
+				var fullyClosed = false;
+				var fullyOpen = false;
+				var alerts = this.state.alerts;
+
+
+				// CLOSED means the reed switch is being actuated (the actuator is close to the sensor)
+				if (data.closedSwitchStatus === "CLOSED") {
+					fullyClosed = true;
+				}
+				if (data.openedSwitchStatus === "CLOSED") {
+					fullyOpen = true;
+				}
+
+				var sensorMalfunction = false;
+				if ((fullyOpen && fullyClosed)) {
+					sensorMalfunction = true;
+				}
+				this.setState({
+					closed: fullyClosed,
+					opened: fullyOpen,
+					alertSensorMalfunction: sensorMalfunction
+				});
+
+			}.bind(this),
+			error: function() {
+				this.setState({alertApiError: true});
+			}.bind(this)
+		});
 	},
 
 	toggle: function(value) {
-		if (this.state.state === "CLOSED") {
-			//this.setState({state: "OPEN"});
-			this.setState({state: "moving"});
-		} else {
-			this.setState({state: "CLOSED"});
+		$.ajax({
+			url: apiUrl + "toggle",
+			type: 'POST',
+			timeout: 5000,
+			error: function(xhr, status, err) {
+				this.setState({alertApiError: true});
+			}.bind(this)
+		});
+	},
+
+	isAlertVisible: function() {
+		return (this.state.alertSensorMalfunction || this.state.alertApiStatusError || this.state.alertApiToggleError);
+	},
+
+	renderAlert: function() {
+		var self = this;
+
+		var renderSensorMalfunctionAlert = function() {
+			if (self.state.alertSensorMalfunction) {
+				return (<li>Garage sensor malfunction.  Both states have been detected.</li>);
+			}
+		};
+
+		var renderApiStatusAlert = function() {
+			if (self.state.alertApiStatusError) {
+				return (<li>Cannot communicate with control device.</li>);
+			}
+		};
+
+		var renderApiToggleAlert = function() {
+			if (self.state.alertApiToggleError) {
+				return (<li>Error toggling garage remote transmission.</li>);
+			}
+		};
+
+		if (this.isAlertVisible()) {
+			return (
+				<Alert bsStyle="danger">
+					<h4>There's a problem!</h4>
+					<ul>
+					{renderSensorMalfunctionAlert()}
+					{renderApiStatusAlert()}
+					</ul>
+					<br />
+					<p>You can still attempt to toggle the garage door, though it isn't guaranteed to work.</p>
+				</Alert>
+				);
 		}
+
 	},
 
 	renderButton: function() {
-		if (this.state.state === "CLOSED") {
+		if (this.state.closed && !this.state.opened) {
 			return (
 				<Button bsStyle="success" bsSize="large" onClick={this.toggle} block>
 					<Image src={garageClosedData} width="50" height="50" />
 				</Button>
 				);
-		} else if (this.state.state === "OPEN") {
+		} else if (this.state.opened && !this.state.closed) {
 			return (
 				<Button bsStyle="danger" bsSize="large" onClick={this.toggle} block>
 					<Image src={garageOpenData} width="50" height="50" />
@@ -59,7 +146,7 @@ var Enclosure = React.createClass({
 		} else {
 			return (
 				<Button bsStyle="warning" bsSize="large" onClick={this.toggle} block>
-					<i>DOOR MOVING</i>
+					<Image src={garageOpenData} width="50" height="50" />
 				</Button>
 				);
 		}
@@ -81,10 +168,8 @@ var Enclosure = React.createClass({
 			<div className="container-fluid" ref="parentContainer">				
 				<div className="row">
 					<div className="col-sm-4">
-						<Grid>
 						<PageHeader>Garage Control</PageHeader>
-						</Grid>
-
+						{this.renderAlert()}
 						<Grid>
 							<Row>
 								<Image src={this.state.url} responsive/>
@@ -95,6 +180,8 @@ var Enclosure = React.createClass({
 							<Row>
 								{this.renderDescription()}
 							</Row>
+							<Row>&nbsp;</Row>
+							<Row><Col xsOffset={11}><Button><Glyphicon glyph="cog" /></Button></Col></Row>
 						</Grid>
 					</div>
 				</div>
